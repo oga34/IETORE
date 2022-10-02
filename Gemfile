790cf0f642285ea0f0105370279b1c880ddf9c3b
EC2 Default User <ec2-user@ip-172-31-20-1.ap-northeast-1.compute.internal>
2022-10-02 15:05:40 +0000

    コントローラー・モデルの追加

--- Gemfile
+++ Gemfile
@@ -54,3 +54,14 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'devise'
+
+gem "enum_help"
+
+gem 'kaminari','~> 1.2.1'
+
+gem 'net-smtp'
+# ついでに以下の2つも追加しておく方が良さそう
+gem 'net-imap'
+gem 'net-pop'
---
+++ db/migrate/20221002142244_create_posts.rb
@@ -0,0 +1,15 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      
+      t.integer :user_id, null: false
+      t.integer :genre_id, null: false
+      t.integer :count
+      t.integer :time
+      t.text :body
+      t.integer :status, null: false, default: false
+
+      t.timestamps
+    end
+  end
+end
