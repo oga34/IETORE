f31f0a5dbfb5aedacf97a46e8b2a57b7020d6512
EC2 Default User <ec2-user@ip-172-31-20-1.ap-northeast-1.compute.internal>
2022-10-05 05:47:50 +0000

    ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,39 @@
 Rails.application.routes.draw do
+ root to: 'public/homes#top'
+  
+ get "search" => "posts#search"
+ 
+ namespace :admin do
+  resources :users, only: [:index,:show,:edit,:update]
+ end
+  
+ namespace :admin do
+  resources :genres, only: [:index,:create,:edit,:update]
+ end
+  
+ namespace :admin do
+  root to: 'homes#top'
+ end
+  
+　get 'users/my_page' => 'public/users#show', as: 'my_page'
+  get 'users/information/edit' => 'public/users#edit', as: 'information_edit'
+  patch  'users/information' => 'public/users#update', as: 'information_update'
+  get 'users/unsubcribe' => 'public/users#unsubcribe'
+  patch 'users/withdraw' => 'public/users#withdraw'
+    
+ scope module: :public do
+   get 'posts/draft' => 'posts/draft'
+   resources :posts
+ end
+   
+ scope module: :public do
+   resources :posts_comments
+ end
+   
+ scope module: :public do
+   resources :favorites, only: [:create,:destroy]
+ end
+
 # ユーザー用
 # URL /users/sign_in ...
 devise_for :users,skip: [:passwords], controllers: {
